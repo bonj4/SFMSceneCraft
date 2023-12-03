e514f73d20ec5b5930a9dd1ec4cc2abad5166dab
ömer faruk karaboğa <omerfarukkaraboga@gmail.com>
2023-12-03 14:13:44 +0300

    refactor according to new structure

--- app/data.py
+++ app/data.py
@@ -4,42 +4,35 @@ import numpy as np
 import os
 
 class dataset():
-    def __init__(self,model_name="statue"):
-        
-        self.sequences_dir = 'data/{}/'.format(model_name)
-        
-        self.img=(cv2.imread(self.sequences_dir+"images/"+i,0) for i in sorted(os.listdir(self.sequences_dir+"images")) if i.endswith("JPG"))
+    def __init__(self,data_path:str):
 
-        ''
+        # deprecated
+        # self.sequences_dir = 'data/{}/'.format(model_name)
+        self.sequences_dir=data_path
+
+        self.img=(cv2.imread(self.sequences_dir+"images/"+i,) for i in sorted(os.listdir(self.sequences_dir+"images")) if i.endswith("JPG"))
+        self.num_frame = len(os.listdir(self.sequences_dir+"images"))
         self.K=self.get_camera_intrinsic_params(self.sequences_dir+'cameras.txt')
         
     def next_imgs(self):
         return cv2.rotate(next(self.img),cv2.ROTATE_90_CLOCKWISE)
     def reset_images(self):
         self.img=(cv2.imread(self.sequences_dir+"images/"+i,0) for i in sorted(os.listdir(self.sequences_dir+"images")) if i.endswith("JPG"))
-    def get_camera_intrinsic_params(self,path):
-        K = []
-        with open(path) as f:
-            lines = f.readlines()
-            calib_info = [float(val) for val in lines[0].split(' ')]
-            row1 = [calib_info[0], calib_info[1], calib_info[2]]
-            row2 = [calib_info[3], calib_info[4], calib_info[5]]
-            row3 = [calib_info[6], calib_info[7], calib_info[8]]
-
-            K.append(row1)
-            K.append(row2)
-            K.append(row3)
-        return K
-    # def get_pinhole_intrinsic_params():
+    # def get_camera_intrinsic_params(self,path):
     #     K = []
-    #     with open(calibration_file_dir + '/camera_observatory.txt') as f:
+    #     with open(path) as f:
     #         lines = f.readlines()
+    #         print(lines)
     #         calib_info = [float(val) for val in lines[0].split(' ')]
-    #         row1 = [calib_info[0], 0, calib_info[2]]
-    #         row2 = [0, calib_info[1], calib_info[3]]
-    #         row3 = [0, 0, 1]
-
+    #         row1 = [calib_info[0], calib_info[1], calib_info[2]]
+    #         row2 = [calib_info[3], calib_info[4], calib_info[5]]
+    #         row3 = [calib_info[6], calib_info[7], calib_info[8]]
+    #
     #         K.append(row1)
     #         K.append(row2)
     #         K.append(row3)
-    
+    #     return K
+    def get_camera_intrinsic_params(self,path):
+        self.k_flatten=pd.read_csv(path, delimiter=' ', header=None, index_col=0)
+        return np.array(self.k_flatten).reshape((3,3))
+
