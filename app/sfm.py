01ef1c161fe0b8907d1ed5582530df3a5c07525a
ömer faruk karaboğa <omerfarukkaraboga@gmail.com>
2024-01-14 18:48:46 +0300

    get command to ba, it takes too much time

--- app/sfm.py
+++ app/sfm.py
@@ -56,9 +56,11 @@ class SFM():
                 curr_img = self.data.next_imgs()
                 # Extract features
                 prev_kp, prev_des = extract_features(
-                    prev_img, detector=self.detector, GoodP=self.GoodP, )
+                    prev_img, detector=self.detector, GoodP=self.GoodP, sift_peak_threshold=self.sift_peak_threshold,
+                    edgeThreshold=self.sift_edge_threshold,min_features=self.min_features)
                 curr_kp, curr_des = extract_features(
-                    curr_img, detector=self.detector, GoodP=self.GoodP, )
+                    curr_img, detector=self.detector, GoodP=self.GoodP, sift_peak_threshold=self.sift_peak_threshold,
+                    edgeThreshold=self.sift_edge_threshold,min_features=self.min_features)
                 # extract matches
                 matches_unfilter = match_features(
                     des1=prev_des, des2=curr_des, matching=self.matching, detector=self.detector, )
@@ -92,34 +94,39 @@ class SFM():
                 print("observed point: ", filtered_points_3d.shape, " 3d points: ", self.pts_4d.shape)
 
                 # Bundle adjustment TODO make pure and understoodable
-                n_cameras, n_points, curr_points = len(self.poses), len(self.pts_4d), len(filtered_points_3d)
-                if idx == 1:
-                    self.points_2d = point1[filter_pts]
-                    self.camera_indices.extend([0] * n_points)
-                    self.point_indices.extend(np.arange(n_points))
-
-                self.camera_indices.extend([idx] * curr_points)
-                self.point_indices.extend(np.arange(curr_points))
-                self.points_2d = np.vstack((self.points_2d, point2[filter_pts]))
-
-                self.camera_parameters[idx, :] = proj_mat_to_camera_vec(self.P2)
-
-                x0 = np.hstack(
-                    (self.camera_parameters[:idx + 1].ravel(), self.pts_4d.ravel()))
-                A = bundle_adjustment_sparsity(n_cameras, n_points, np.array(self.camera_indices),
-                                               np.array(self.point_indices))
-                res = least_squares(fun, x0, jac='3-point', jac_sparsity=A, verbose=1, x_scale='jac', ftol=1e-5,
-                                    method='trf', loss='soft_l1',
-                                    args=(
-                                        n_cameras, n_points, np.array(self.camera_indices),
-                                        np.array(self.point_indices),
-                                        self.points_2d,
-                                        self.data.K))
-
+                # n_cameras, n_points, curr_points = len(self.poses), len(self.pts_4d), len(filtered_points_3d)
+                # if idx == 1:
+                #     self.points_2d = point1[filter_pts]
+                #     self.camera_indices.extend([0] * n_points)
+                #     self.point_indices.extend(np.arange(n_points))
+                #
+                # self.camera_indices.extend([idx] * curr_points)
+                # self.point_indices.extend(np.arange(curr_points))
+                # self.points_2d = np.vstack((self.points_2d, point2[filter_pts]))
+                #
+                # self.camera_parameters[idx, :] = proj_mat_to_camera_vec(self.P2)
+                #
+                # x0 = np.hstack(
+                #     (self.camera_parameters[:idx + 1].ravel(), self.pts_4d.ravel()))
+                # A = bundle_adjustment_sparsity(n_cameras, n_points, np.array(self.camera_indices),
+                #                                np.array(self.point_indices))
+                # res = least_squares(fun, x0, jac='3-point', jac_sparsity=A, verbose=1, x_scale='jac', ftol=1e-5,
+                #                     method='trf', loss='soft_l1',
+                #                     args=(
+                #                         n_cameras, n_points, np.array(self.camera_indices),
+                #                         np.array(self.point_indices),
+                #                         self.points_2d,
+                #                         self.data.K))
+                # optimized_params = res.x
+                # camera_params = optimized_params[:n_cameras * 6].reshape((n_cameras, 6))
+                # self.pts_4d = optimized_params[n_cameras * 6:].reshape((n_points, 3))
+                # for idx, camera_param in enumerate(camera_params):
+                #     self.poses[idx] = recover_projection_matrix(camera_param)
+                #     self.camera_parameters[idx, :] = camera_param
                 self.disp.paint(self.poses, self.pts_4d, self.colors_4d)
                 prev_img = curr_img
+                # self.P1 = np.copy(recover_projection_matrix(camera_params[-1]))
                 self.P1 = np.copy(self.P2)
-
 
 
 if __name__ == "__main__":
